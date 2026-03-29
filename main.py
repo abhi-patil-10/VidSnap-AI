0f36e8b44a7d4f462e5edb3a61b4ff8295a97679
Abhijeet Patil <abhigithub777@gmail.com>
2026-03-30 00:36:31 +0530

    start a work Collecting a user uploaded files (Assign unique id for every user to store their data)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import uuid
 
 app = Flask(__name__)
 
@@ -6,9 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/create")
+@app.route("/create",methods=["GET","POST"])
 def create():
-    return render_template("create.html")
+    my_id = uuid.uuid1()
+    if request.method == "POST":
+        for key , values in request.files.items():
+            print(key,values)
+    return render_template("create.html" , my_id = my_id)
 
 @app.route("/gallery")
 def gallery():
